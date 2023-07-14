9f92e34679a375c1224cf49835b16f2e703538fd
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-15 01:47:46 +0530

    improved UI of profile route

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,17 @@ import React, { FunctionComponent, useEffect, useState } from "react";
 import { db } from "@/lib/firebaseStore";
 import { query, where, collection, getDocs } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { EyeNoneIcon, PersonIcon } from "@radix-ui/react-icons";
 
 interface OwnProps {}
 
@@ -33,10 +44,41 @@ const Page: FunctionComponent<Props> = (props) => {
 
   return (
     <div className={"flex flex-col justify-center items-center h-screen"}>
-      <h1>Profile</h1>
-      <p>{user?.name}</p>
-      <p>{user?.email}</p>
-      <p>{user?.paymentId}</p>
+      <Card>
+        <CardHeader>
+          <CardTitle>Profile</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-1">
+          <div className="-mx-2 flex items-start space-x-4 rounded-md bg-accent p-2 text-accent-foreground transition-all">
+            <PersonIcon className="mt-px h-5 w-5" />
+            <div className="space-y-1">
+              <p className="text-sm font-medium leading-none">{user?.name}</p>
+              <p className="text-sm text-muted-foreground">{user?.email}</p>
+            </div>
+          </div>
+
+          {user?.paymentId && (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <EyeNoneIcon className="mt-px h-5 w-5" />
+              <div className="space-y-1">
+                <p className="text-sm font-medium leading-none">Payment ID</p>
+                <p className="text-sm text-muted-foreground">
+                  {user?.paymentId}
+                </p>
+              </div>
+            </div>
+          )}
+        </CardContent>
+        {!user?.paymentId && (
+          <CardFooter>
+            <Link href={"/pricing"} className={"w-full"}>
+              <Button variant={"default"} className={"w-full"}>
+                Get Ticket
+              </Button>
+            </Link>
+          </CardFooter>
+        )}
+      </Card>
     </div>
   );
 };
