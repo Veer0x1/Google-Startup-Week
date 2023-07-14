580d2e9d30b00296de01b368e7b5f92a8d010085
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-14 12:26:33 +0530

    added loading skeleton

--- app/checkout/success/page.tsx
+++ app/checkout/success/page.tsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
-import { db }from "@/lib/firebaseStore";
-import { doc,setDoc, collection } from "firebase/firestore";
+import { db } from "@/lib/firebaseStore";
+import { doc, setDoc } from "firebase/firestore";
+import { Skeleton } from "@/components/ui/skeleton";
 //@ts-ignore
 const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
 
@@ -15,9 +16,9 @@ const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
 // }
 
 export default function SuccessPage() {
-//   const [paymentDetails, setPaymentDetails] = useState<
-//     CheckoutSessionData | {}
-//   >({});
+  //   const [paymentDetails, setPaymentDetails] = useState<
+  //     CheckoutSessionData | {}
+  //   >({});
 
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("session_id");
@@ -25,10 +26,7 @@ export default function SuccessPage() {
 
   const URL = sessionId ? `/api/stripe/sessions/${sessionId}` : null;
 
-  const { data: checkoutSession, error } = useSWR(
-    URL,
-    fetcher
-  );
+  const { data: checkoutSession, error } = useSWR(URL, fetcher);
 
   if (error) return <div>failed to load the session</div>;
 
@@ -36,24 +34,35 @@ export default function SuccessPage() {
   if (checkoutSession) {
     // setPaymentDetails(checkoutSession);
 
-    const docId = checkoutSession.customer_details?.name + checkoutSession.customer_details?.email;
+    const docId =
+      checkoutSession.customer_details?.name +
+      checkoutSession.customer_details?.email;
 
-    setDoc(doc(db,"payments",docId),checkoutSession)
-
+    setDoc(doc(db, "payments", docId), checkoutSession);
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="">
-          Your payment of {checkoutSession.payment_intent.amount_received / 100} is successful
+          Your payment of {checkoutSession.payment_intent.amount_received / 100}{" "}
+          is successful
         </h1>
         <h2>Customer details</h2>
         <p>Payment Id: {checkoutSession.payment_intent.id}</p>
         <p>Customer Name: {checkoutSession.customer_details?.name}</p>
         <p>Customer Email: {checkoutSession.customer_details?.email}</p>
       </div>
-      
     );
   } else {
-    return <div className="flex flex-col justify-center h-screen items-center">loading...</div>;
+    return (
+      <div className="flex flex-col justify-center h-screen items-center space-x-4">
+        <Skeleton className="h-4 w-[300px]" />
+        <div className="space-y-2 mt-8">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    );
   }
 }
